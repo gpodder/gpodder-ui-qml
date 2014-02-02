e6869016f21e1f59c3a12daf9f3e3d463e0875ca
Thomas Perl <m@thp.io>
2014-02-02 01:08:26 +0100

    util.js: Add time formatting functions from gPodder 3's QML UI

--- common/util.js
+++ common/util.js
@@ -42,3 +42,22 @@ function updateModelWith(model, key, value, update) {
         }
     }
 }
+
+function formatDuration(duration) {
+    if (duration !== 0 && !duration) {
+        return ''
+    }
+
+    var h = parseInt(duration / 3600) % 24
+    var m = parseInt(duration / 60) % 60
+    var s = parseInt(duration % 60)
+
+    var hh = h > 0 ? (h < 10 ? '0' + h : h) + ':' : ''
+    var ms = (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s)
+
+    return hh + ms
+}
+
+function formatPosition(position,duration) {
+  return formatDuration(position) + " / " + formatDuration(duration)
+}
